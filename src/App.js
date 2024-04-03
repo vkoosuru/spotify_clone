da3d2640437fbc51cf357c7bf53b4c855cc5a0ee
vkoosuru <koosuruvardhini@gmail.com>
2024-04-03 12:14:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 function App() {
+  const [token,setToken]=useState(null);
+  useEffect(()=>{
+    const hash=getTokenFromUrl();
+    window.location.hash="";
+    const _token =hash.access_token;
+    if(_token){
+      setToken(_token)
+    }
+    console.log("I have a token ",token);
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token?(
+          <h1> I am logged in</h1>
+        ):(
+          <Login />
+        )
+      }
     </div>
   );
 }
